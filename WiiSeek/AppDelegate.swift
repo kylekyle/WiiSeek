28d1cfd0ef04ede0ced64cdd144b872f6b72d565
Kyle King <kylejking@gmail.com>
2019-06-16 13:41:34 -0400

    Initial Commit

---
+++ WiiSeek/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WiiSeek
+//
+//  Created by Kyle King on 6/16/19.
+//  Copyright © 2019 Kyle King. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WiiSeek/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WiiSeek
+//
+//  Created by Kyle King on 6/16/19.
+//  Copyright © 2019 Kyle King. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
